0d60f2699cce51f60bc46f7d00b7bb598a5d5c89
xin <oncwnuN1VCJ08Bt_EFHQkrbYfrSE@git.weixin.qq.com>
2024-04-02 17:06:52 +0800

    add learn page & some componets

---
+++ app/(main)/learn/page.tsx
@@ -0,0 +1,27 @@
+import { Header } from "./header"
+import { FeedWrapper } from "@/components/feed-wrapper"
+import { UserProgress } from "@/components/user-progress"
+import { StickyWrapper } from "@/components/sticky-wrapper"
+
+const LearnPage = () => {
+    return (
+        <div className="flex flex-row-reverse gap-[48px] px-6">
+            <StickyWrapper>
+                <UserProgress
+                    activeCourse={{
+                        title: "Spanish",
+                        imageSrc: "/es.svg"
+                    }}
+                    hearts={5}
+                    points={100}
+                    hasActiveSubscription={false}
+                />
+            </StickyWrapper>
+            <FeedWrapper>
+                <Header title="Spanish" />
+            </FeedWrapper>
+        </div>
+    )
+}
+
+export default LearnPage
