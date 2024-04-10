06e44b7f6bf15d09b53ebff7ac3fb4a0c90da908
xin <oncwnuN1VCJ08Bt_EFHQkrbYfrSE@git.weixin.qq.com>
2024-04-10 11:48:02 +0800

    build lesson

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,20 +1,28 @@
-import { Header } from "./header"
+import { redirect } from "next/navigation"
+import { getUnits, getUserProgress, getCourseProgress, getLessonPercentage } from "@/db/queries"
+
 import { FeedWrapper } from "@/components/feed-wrapper"
 import { UserProgress } from "@/components/user-progress"
 import { StickyWrapper } from "@/components/sticky-wrapper"
-import { getUnits, getUserProgress } from "@/db/queries"
-import { redirect } from "next/navigation"
+
+import { Unit } from "./unit"
+import { Header } from "./header"
+import { lessons, units as unitsSchema } from "@/db/schema"
 
 const LearnPage = async () => {
     const userProgressData = getUserProgress()
+    const courseProgressData = getCourseProgress()
+    const lessonPercentageData = getLessonPercentage()
     const unitsData = getUnits()
 
-    const [userProgress, units] = await Promise.all([userProgressData, unitsData])
+    const [userProgress, units, courseProgress, lessonPercentage] = await Promise.all([userProgressData, unitsData, courseProgressData, lessonPercentageData])
 
     if (!userProgress || !userProgress.activeCourse) {
         redirect("/courses")
         // 这里自动 return 了
     }
+
+    if (!courseProgress) redirect("/courses")
 
     return (
         <div className="flex flex-row-reverse gap-[48px] px-6">
@@ -30,7 +38,17 @@ const LearnPage = async () => {
                 <Header title={userProgress.activeCourse.title} />
                 {units.map(unit => (
                     <div key={unit.id} className="mb-10">
-                        {JSON.stringify(unit)}
+                        <Unit
+                            id={unit.id}
+                            order={unit.order}
+                            description={unit.description}
+                            title={unit.title}
+                            lessons={unit.lessons}
+                            activeLesson={courseProgress.activeLesson as typeof lessons.$inferSelect & {
+                                unit: typeof unitsSchema.$inferSelect
+                            } | undefined}
+                            activeLessonPercentage={lessonPercentage}
+                        />
                     </div>
                 ))}
             </FeedWrapper>
--- db/queries.ts
+++ db/queries.ts
@@ -4,7 +4,7 @@ import { eq } from "drizzle-orm";
 import { auth } from "@clerk/nextjs";
 
 import db from "@/db/drizzle";
-import { challengeProgress, courses, units, userProgress } from "@/db/schema";
+import { challengeProgress, courses, lessons, units, userProgress } from "@/db/schema";
 
 export const getCourses = cache(async () => {
     const data = await db.query.courses.findMany()
@@ -57,6 +57,7 @@ export const getUnits = cache(async () => {
     // 找到所有挑战都完成的 lesson
     const normalizedData = data.map((unit) => {
         const lessonsWithCompletedStatus = unit.lessons.map((lesson) => {
+            if (lesson.challenges.length === 0) return { ...lesson, completed: false }
             const allCompletedChallenges = lesson.challenges.every((challenge) => {
                 return challenge.challengeProgress
                     && challenge.challengeProgress.length > 0
@@ -69,3 +70,114 @@ export const getUnits = cache(async () => {
 
     return normalizedData
 })
+
+export const getCourseProgress = cache(async () => {
+    const { userId } = await auth();
+    const userProgress = await getUserProgress();
+
+    if (!userId || !userProgress?.activeCourseId) {
+        return null;
+    }
+
+    const unitsInActiveCourse = await db.query.units.findMany({
+        orderBy: (units, { asc }) => [asc(units.order)],
+        where: eq(units.courseId, userProgress.activeCourseId),
+        with: {
+            lessons: {
+                orderBy: (lessons, { asc }) => [asc(lessons.order)],
+                with: {
+                    unit: true,
+                    challenges: {
+                        with: {
+                            challengeProgress: {
+                                where: eq(challengeProgress.userId, userId),
+                            },
+                        },
+                    },
+                },
+            },
+        },
+    });
+
+    // 寻找第一个未完成的课时
+    const firstUncompletedLesson = unitsInActiveCourse
+        // flatMap() 函数用于将每个课程单元的课程（lessons）抽取出来，并将它们合并为一个新的扁平化的数组。
+        .flatMap((unit) => unit.lessons)
+        .find((lesson) => {
+            return lesson.challenges.some((challenge) => {
+                return !challenge.challengeProgress
+                    || challenge.challengeProgress.length === 0
+                    || challenge.challengeProgress.some((progress) => progress.completed === false)
+            });
+        });
+
+    return {
+        activeLesson: firstUncompletedLesson,
+        activeLessonId: firstUncompletedLesson?.id,
+    };
+});
+
+export const getLesson = cache(async (id?: number) => {
+    const { userId } = await auth();
+
+    if (!userId) return null;
+
+    const courseProgress = await getCourseProgress();
+    const lessonId = id || courseProgress?.activeLessonId;
+
+    if (!lessonId) return null;
+
+    const data = await db.query.lessons.findFirst({
+        where: eq(lessons.id, lessonId),
+        with: {
+            challenges: {
+                orderBy: (challenges, { asc }) => [asc(challenges.order)],
+                with: {
+                    challengeOptions: true,
+                    challengeProgress: {
+                        where: eq(challengeProgress.userId, userId),
+                    },
+                },
+            },
+        },
+    });
+
+    if (!data || !data.challenges) {
+        return null;
+    }
+
+    // 计算每个挑战是否已完成
+    const normalizedChallenges = data.challenges.map((challenge) => {
+        const completed = challenge.challengeProgress
+            && challenge.challengeProgress.length > 0
+            && challenge.challengeProgress.every((progress) => progress.completed)
+
+        return { ...challenge, completed };
+    });
+
+    // 包含课程信息和处理后的挑战信息的对象
+    return { ...data, challenges: normalizedChallenges }
+});
+
+export const getLessonPercentage = cache(async () => {
+    const courseProgress = await getCourseProgress();
+
+    if (!courseProgress?.activeLessonId) {
+        return 0;
+    }
+
+    // 返回某个课时的
+    const lesson = await getLesson(courseProgress.activeLessonId);
+
+    if (!lesson) {
+        return 0;
+    }
+
+    const completedChallenges = lesson.challenges
+        .filter((challenge) => challenge.completed);
+    const percentage = Math.round(
+        (completedChallenges.length / lesson.challenges.length) * 100,
+    );
+
+    return percentage;
+});
