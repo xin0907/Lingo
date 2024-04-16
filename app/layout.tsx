7c4320598b1f01532e5a1ec74b3635f2f53275d4
xin <oncwnuN1VCJ08Bt_EFHQkrbYfrSE@git.weixin.qq.com>
2024-04-16 23:08:53 +0800

    deployment

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
-import { Nunito } from "next/font/google";
 import { ClerkProvider } from '@clerk/nextjs'
+
+// import { nunito } from "@/app/ui/font";
 
 import { Toaster } from "@/components/ui/sonner";
 import { ExitModal } from "@/components/modals/exit-modal";
@@ -8,8 +9,6 @@ import { HeartsModal } from "@/components/modals/hearts-modal";
 import { PracticeModal } from "@/components/modals/practice-modal";
 
 import "./globals.css";
-
-const font = Nunito({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -24,7 +23,8 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={font.className}>
+        {/* className={nunito.className} */}
+        <body>
           <Toaster />
           <ExitModal />
           <HeartsModal />
